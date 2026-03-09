826cdcd3d9d9d17f610b92ff8f5dab0215b75dc8
raphaeltaurai <shawntaurai@gmail.com>
2026-03-09 22:41:37 +0200

    .

---
+++ src/TodoItem.jsx
@@ -0,0 +1,10 @@
+export function TodoItem({completed, id, title, toggleTodo, deleteTodo
+    
+}) {
+    return(<li>
+        <label>
+          <input type="checkbox" checked={completed}/>
+          onChange={e => toggleTodo(id, e.target.checked)}{title}</label>
+        < button onClick={() => (id)}className="btn btn-danger">Delete</button>
+      </li>)
+}
